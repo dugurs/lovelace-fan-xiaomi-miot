b9b631e6f23c3eee010cf8155cc8ba1617126a53
dugurs <41262994+dugurs@users.noreply.github.com>
2022-06-20 23:12:28 +0900

    Add files via upload

--- fan-xiaomi-miot.js
+++ fan-xiaomi-miot.js
@@ -14,187 +14,310 @@ class fanXiaomiMiotCard extends LitElement {
     };
   }
 
-  btns = [
-    {
-      name: 'power',
-      prop: 'fan.on',
-      value: false,
-      state: ['off', 'on'],
-      icon: 'mdi:fan'
-    },
-    {
-      name: 'mode',
-      prop: 'fan.mode',
-      value: [0,1,2,3],
-      state: ['Straight Wind', 'Natural Wind', 'Smart', 'Sleep'],
-      icon: ['mdi:weather-windy', 'mdi:leaf', 'mdi:fan-auto', 'mdi:power-sleep']
-    },
-    {
-      name: 'speed',
-      prop: 'fan.fan_level',
-      value: [1,2,3,4],
-      icon: ['mdi:numeric-1-box-outline','mdi:numeric-2-box-outline','mdi:numeric-3-box-outline','mdi:numeric-4-box-outline']
-    },
-    {
-      name: 'off_delay_time',
-      prop: 'off_delay_time',
-      value: [0,30,60,120,180,240,300,360],
-      icon: 'mdi:camera-timer'
-    },
-    {
-      name: 'hswing',
-      prop: 'fan.horizontal_swing',
-      value: false,
-      state: ['off', 'on'],
-      icon: 'mdi:arrow-left-right'
-    },
-    {
-      name: 'hswing_angle',
-      prop: 'horizontal_swing_included_angle-2-5',
-      value: [30,60,90,120,140],
-      icon: 'mdi:arrow-left-right',
-      // click: 'hswing',
-      // dblclick: 'hswing_angle'
-    },
-    {
-      name: 'vswing',
-      prop: 'fan.vertical_swing',
-      value: false,
-      state: ['off', 'on'],
-      icon: 'mdi:arrow-up-down'
-    },
-    {
-      name: 'vswing_angle',
-      prop: 'vertical_swing_included_angle-2-8',
-      value: [35,65,95],
-      icon: 'mdi:arrow-up-down',
-      // click: 'vswing',
-      // dblclick: 'vswing_angle'
-    },
-    {
-      name: 'swing_down',
-      prop: 'dm_service.swing_updown_manual',
-      value: 2,
-      icon: 'mdi:chevron-down',
-    },
-    {
-      name: 'swing_up',
-      prop: 'dm_service.swing_updown_manual',
-      value: 1,
-      icon: 'mdi:chevron-up',
-    },
-    {
-      name: 'swing_left',
-      prop: 'dm_service.swing_lr_manual',
-      value: 1,
-      icon: 'mdi:chevron-left',
-    },
-    {
-      name: 'swing_right',
-      prop: 'dm_service.swing_lr_manual',
-      value: 2,
-      icon: 'mdi:chevron-right',
-    },
-    {
-      name: 'temperature',
-      prop: 'temperature-9-1',
-      value: 1,
-      icon: 'mdi:thermometer',
-      click: ''
-    },
-    {
-      name: 'humidity',
-      prop: 'relative_humidity-9-2',
-      value: 1,
-      icon: 'mdi:water-percent',
-      click: ''
-    },
-    {
-      name: 'alarm',
-      prop: 'alarm',
-      value: false,
-      state: ['off', 'on'],
-      icon: ['mdi:volume-off','mdi:volume-high']
-    },
-    {
-      name: 'locked',
-      prop: 'physical_controls_locked',
-      value: false,
-      state: ['off', 'on'],
-      icon: ['mdi:lock-open-variant','mdi:lock-open']
-    },
-    {
-      name: 'auto_on',
-      prop: 'dm_service.on',
-      value: false,
-      state: ['off', 'on'],
-      icon: 'mdi:fan-auto',
-      label: 'auto_on_temp_minus'
-    },
-    {
-      name: 'auto_on_temp_minus',
-      prop: 'dm_service.temperature',
-      value: 15,
-      min: 15,
-      max: 36,
-      step: 1,
-      icon: 'mdi:chevron-down',
-      backward: true
-    },
-    {
-      name: 'auto_on_temp_plus',
-      prop: 'dm_service.temperature',
-      value: 15,
-      min: 15,
-      max: 36,
-      step: 1,
-      icon: 'mdi:chevron-up',
-    },
-    {
-      name: 'auto_off',
-      prop: 'dm_service.off',
-      value: false,
-      state: ['off', 'on'],
-      icon: 'mdi:fan-off',
-      label: 'auto_off_temp_minus'
-    },
-    {
-      name: 'auto_off_temp_minus',
-      prop: 'dm_service-8.temperature',
-      value: 14,
-      min: 14,
-      max: 35,
-      step: 1,
-      icon: 'mdi:chevron-down',
-      backward: true
-    },
-    {
-      name: 'auto_off_temp_plus',
-      prop: 'dm_service-8.temperature',
-      value: 14,
-      min: 14,
-      max: 35,
-      step: 1,
-      icon: 'mdi:chevron-up',
-    },
-  ]
 
+  async connectedCallback() {
+    super.connectedCallback()
+    const state = this.hass.states[this.config.entity];
+    await this.setBtns();
+  }
+
+  
+  btns = [];
   btnskey = {};
+  models = {}
+
+  async setBtns() {
+    
+    this.models = {
+      dmaker_fan_p220: [
+        {
+          name: 'power',
+          prop: 'fan.on',
+          value: false,
+          state: ['off', 'on'],
+          icon: 'mdi:fan',
+          label: 'dm_service.speed_level'
+        },
+        // {
+        //   name: 'speed',
+        //   prop: 'fan.fan_level',
+        //   value: [1,2,3,4],
+        //   icon: ['mdi:numeric-1-box-outline','mdi:numeric-2-box-outline','mdi:numeric-3-box-outline','mdi:numeric-4-box-outline']
+        // },
+        {
+          name: 'speed_down',
+          prop: 'dm_service.speed_level',
+          value: 1,
+          min: 1,
+          max: 100,
+          step: this.config.percentage_step ?? 25,
+          icon: 'mdi:chevron-down',
+          backward: true
+        },
+        {
+          name: 'speed_up',
+          prop: 'dm_service.speed_level',
+          value: 1,
+          min: 1,
+          max: 100,
+          step: this.config.percentage_step ?? 25,
+          icon: 'mdi:chevron-up',
+        },
+        {
+          name: 'mode',
+          prop: 'fan.mode',
+          value: [0,1,2,3],
+          state: ['Straight Wind', 'Natural Wind', 'Smart', 'Sleep'],
+          icon: ['mdi:weather-windy', 'mdi:leaf', 'mdi:fan-auto', 'mdi:power-sleep']
+        },
+        {
+          name: 'off_delay_time',
+          prop: 'off_delay_time',
+          value: this.config.off_delay_time ?? [0,30,60,120,180,240,300,360,420,480],
+          icon: 'mdi:camera-timer'
+        },
+        {
+          name: 'hswing',
+          prop: 'fan.horizontal_swing',
+          value: false,
+          state: ['off', 'on'],
+          icon: 'mdi:arrow-left-right'
+        },
+        {
+          name: 'hswing_angle',
+          prop: 'horizontal_swing_included_angle-2-5',
+          value: [30,60,90,120,140],
+          icon: 'mdi:arrow-left-right',
+          // click: 'hswing',
+          // dblclick: 'hswing_angle'
+        },
+        {
+          name: 'vswing',
+          prop: 'fan.vertical_swing',
+          value: false,
+          state: ['off', 'on'],
+          icon: 'mdi:arrow-up-down'
+        },
+        {
+          name: 'vswing_angle',
+          prop: 'vertical_swing_included_angle-2-8',
+          value: [35,65,95],
+          icon: 'mdi:arrow-up-down',
+          // click: 'vswing',
+          // dblclick: 'vswing_angle'
+        },
+        {
+          name: 'swing_down',
+          prop: 'dm_service.swing_updown_manual',
+          value: 2,
+          icon: 'mdi:chevron-down',
+        },
+        {
+          name: 'swing_up',
+          prop: 'dm_service.swing_updown_manual',
+          value: 1,
+          icon: 'mdi:chevron-up',
+        },
+        {
+          name: 'swing_left',
+          prop: 'dm_service.swing_lr_manual',
+          value: 1,
+          icon: 'mdi:chevron-left',
+        },
+        {
+          name: 'swing_right',
+          prop: 'dm_service.swing_lr_manual',
+          value: 2,
+          icon: 'mdi:chevron-right',
+        },
+        {
+          name: 'temperature',
+          prop: 'temperature-9-1',
+          value: 1,
+          icon: 'mdi:thermometer',
+          click: ''
+        },
+        {
+          name: 'humidity',
+          prop: 'relative_humidity-9-2',
+          value: 1,
+          icon: 'mdi:water-percent',
+          click: ''
+        },
+        {
+          name: 'alarm',
+          prop: 'alarm',
+          value: false,
+          state: ['off', 'on'],
+          icon: ['mdi:volume-off','mdi:volume-high']
+        },
+        {
+          name: 'locked',
+          prop: 'physical_controls_locked',
+          value: false,
+          state: ['off', 'on'],
+          icon: ['mdi:lock-open-variant','mdi:lock-open']
+        },
+        {
+          name: 'auto_on',
+          prop: 'dm_service.on',
+          value: false,
+          state: ['off', 'on'],
+          icon: 'mdi:fan-auto',
+          label: 'auto_on_temp_minus'
+        },
+        {
+          name: 'auto_on_temp_minus',
+          prop: 'dm_service.temperature',
+          value: 15,
+          min: 15,
+          max: 36,
+          step: 1,
+          icon: 'mdi:chevron-down',
+          backward: true
+        },
+        {
+          name: 'auto_on_temp_plus',
+          prop: 'dm_service.temperature',
+          value: 15,
+          min: 15,
+          max: 36,
+          step: 1,
+          icon: 'mdi:chevron-up',
+        },
+        {
+          name: 'auto_off',
+          prop: 'dm_service.off',
+          value: false,
+          state: ['off', 'on'],
+          icon: 'mdi:fan-off',
+          label: 'auto_off_temp_minus'
+        },
+        {
+          name: 'auto_off_temp_minus',
+          prop: 'dm_service-8.temperature',
+          value: 14,
+          min: 14,
+          max: 35,
+          step: 1,
+          icon: 'mdi:chevron-down',
+          backward: true
+        },
+        {
+          name: 'auto_off_temp_plus',
+          prop: 'dm_service-8.temperature',
+          value: 14,
+          min: 14,
+          max: 35,
+          step: 1,
+          icon: 'mdi:chevron-up',
+        },
+      ],
+      dmaker_fan_p5: [
+        {
+          name: 'power',
+          prop: 'fan.on',
+          value: false,
+          state: ['off', 'on'],
+          icon: 'mdi:fan',
+          label: 'fan.fan_level'
+        },
+        {
+          name: 'speed_down',
+          prop: 'fan.fan_level',
+          value: 1,
+          min: 1,
+          max: 100,
+          step: this.config.percentage_step ?? 25,
+          icon: 'mdi:chevron-down',
+          backward: true
+        },
+        {
+          name: 'speed_up',
+          prop: 'fan.fan_level',
+          value: 1,
+          min: 1,
+          max: 100,
+          step: this.config.percentage_step ?? 25,
+          icon: 'mdi:chevron-up',
+        },
+        {
+          name: 'mode',
+          prop: 'fan.mode',
+          value: [0,1],
+          state: ['Natural Wind', 'Straight Wind'],
+          icon: ['mdi:leaf','mdi:weather-windy']
+        },
+        {
+          name: 'off_delay_time',
+          prop: 'off_delay_time',
+          value: this.config.off_delay_time ?? [0,30,60,120,180,240,300,360],
+          icon: 'mdi:camera-timer'
+        },
+        {
+          name: 'hswing',
+          prop: 'fan.horizontal_swing',
+          value: false,
+          state: ['off', 'on'],
+          icon: 'mdi:arrow-left-right'
+        },
+        {
+          name: 'hswing_angle',
+          prop: 'fan.horizontal_angle',
+          value: [30,60,90,120,140],
+          icon: 'mdi:arrow-left-right',
+        },
+        {
+          name: 'swing_left',
+          prop: 'dm_service.swing_lr_manual',
+          value: 1,
+          icon: 'mdi:chevron-left',
+        },
+        {
+          name: 'swing_right',
+          prop: 'dm_service.swing_lr_manual',
+          value: 2,
+          icon: 'mdi:chevron-right',
+        },
+        {
+          name: 'alarm',
+          prop: 'alarm',
+          value: false,
+          state: ['off', 'on'],
+          icon: ['mdi:volume-off','mdi:volume-high']
+        },
+        {
+          name: 'locked',
+          prop: 'physical_controls_locked',
+          value: false,
+          state: ['off', 'on'],
+          icon: ['mdi:lock-open-variant','mdi:lock-open']
+        },
+      ]
+    }
+
+    const model = this.config.model ?? 'dmaker_fan_p220';
+    this.btns = this.models[model];
+  
+    for (let key in this.btns) {
+      this.btnskey[this.btns[key].name] = Number(key);
+    }
+  }
+
+  
+
 
   render() {
     const entityId = this.config.entity;
     const state = this.hass.states[entityId];
     const stateStr = state ? state.state : 'unavailable';
-    for (let key in this.btns) {
-      this.btnskey[this.btns[key].name] = Number(key);
-    }
-    // console.log('btnskey:', this.btnskey);
+    const model = this.config.model ?? 'p220';
     if (stateStr == 'unavailable') {
       return html`<div class="not-found">Entity ${entityId} not found.</div>`;
     } else {
       const hideTitle = this.config?.hide_title ? 'hide' : '';
       return html`
-        <ha-card>
+        <ha-card class="${model}">
           <div class="title ${hideTitle}">
             ${state.attributes.friendly_name}
           </div>
@@ -274,20 +397,11 @@ class fanXiaomiMiotCard extends LitElement {
     if (typeof btn.label == 'undefined') {
       return '';
     }
-    let i;
-    if (typeof this.btnskey[btn.label] != 'undefined') {
-      btn = this.btns[this.btnskey[btn['label']]];
-      if (typeof btn.value == 'boolean') {
-        i = state.attributes[btn.prop] ? 1 : 0;
-        return btn.state[i];
-      } else if (typeof btn.state == 'object') {
-        i = btn.value.indexOf(state.attributes[btn.prop]);
-        return btn.state[i];
-      } else {
-        return state.attributes[btn.prop];
-      }
-    } else {
+    if (typeof this.btnskey[btn.label] == 'undefined') {
       return state.attributes[btn.label];
+    } else {
+      btn = this.btns[this.btnskey[btn['label']]];
+      return this.setState(state, btn);
     }
   }
   _toggle(type, state, btn) {
@@ -300,6 +414,7 @@ class fanXiaomiMiotCard extends LitElement {
     if (typeof btn[type] != 'undefined') {
       btn = this.btns[this.btnskey[btn[type]]];
     }
+
     let _field = btn.prop;
     let _value = '';
     if (typeof btn.value == 'boolean') {
@@ -310,8 +425,10 @@ class fanXiaomiMiotCard extends LitElement {
       } else {
         _value = state.attributes[btn.prop];
         _value += (btn.backward == true) ? -btn.step : btn.step;
-        if (_value > btn.max || _value < btn.min) {
-          return;
+        if (_value > btn.max ) {
+          _value = btn.max;
+        } else if(_value < btn.min) {
+          _value = btn.min;
         }
       }
     } else if (typeof btn.value == 'object') {
@@ -327,7 +444,7 @@ class fanXiaomiMiotCard extends LitElement {
       _value = btn.value;
     }
     // console.log(type, btn.name, _field, _value);
-    this.hass.callService("xiaomi_miot", "set_property", {
+    this.hass.callService('xiaomi_miot', "set_property", {
       entity_id: state.entity_id,
       field: _field,
       value: _value
@@ -351,19 +468,29 @@ class fanXiaomiMiotCard extends LitElement {
     return css`
       :host {
       }
+      @keyframes rotate_image{
+        100% {
+            transform: rotate(360deg);
+        }
+      }
       ha-card {
-        background: var( --ha-card-background, var(--card-background-color, white) );
-        border-radius: var(--ha-card-border-radius, 4px);
-        box-shadow: var( --ha-card-box-shadow, 0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12) );
-        color: var(--primary-text-color);
-        padding: 10px;
+        background: var(--card-background, var(--card-background-color, white) );
+        border-radius: var(--card-border-radius, 4px);
+        box-shadow: var(--card-box-shadow, 0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12) );
+        color: var(--card-primary-text-color, --primary-text-color);
+        padding: var(--card-padding, 10px);
         display: grid;
-        grid-template-rows: min-content, 30px, 30px;
-        grid-template-columns: repeat(5, 1fr);
-        grid-template-areas: "n n n n n" "p s hs ha t" "o m vs va h" "ao aot af aft a";
-        gap: 6px 6px;
+        grid-template-rows: var(--card--grid-rows, min-content, 30px, 30px, 30px);
+        grid-template-columns: var(--card--grid-columns, repeat(5, 1fr));
+        grid-template-areas: var(--card--grid-areas, "n n n n n" "p s hs ha t" "o m vs va h" "ao aot af aft a");
+        gap: var(--card-grid-gap, 6px 6px);
         align-items: center;
         place-content: space-between;
+      }
+      ha-card.dmaker_fan_p5 {
+        grid-template-rows: var(--card--grid-rows, min-content, 30px);
+        grid-template-columns: var(--card--grid-columns, repeat(7, 1fr));
+        grid-template-areas: var(--card--grid-areas, "n n n n n n n" "p s hs ha o m a");
       }
       ha-card>div {
         display: flex;
@@ -376,21 +503,27 @@ class fanXiaomiMiotCard extends LitElement {
         height: 100%;
         min-height: 40px;
         cursor: pointer;
-        transition: all 0.2s;
+        transition: all 0.4s;
       }
       ha-card>div:active {
-        background-color: var(--box-active-background-color, rgba(127,127,0, 0.4));
+        background-color: var(--box-active-background-color, rgba(127,127,0, 0.3));
       }
       ha-card>div.active {
         background-color: var(--box-active-background-color, rgba(127,127,0, 0.3));
       }
-      ha-card>div.active .icon-waper ha-icon {
+      ha-card>div.active .icon-waper {
         color: var(--icon-active-color, --primary-text-color);
+      }
+      ha-card>div.power.active .icon-waper {
+        ---animation: rotate_image 1.5s linear infinite;
+        ---transform-origin: 50% 50%;
       }
       .title {        grid-area: n; }
       .power {        grid-area: p; }
       .mode {         grid-area: m; }
       .speed {        grid-area: s; }
+      .speed_down {        grid-area: s; position: relative; width: 50%; }
+      .speed_up {        grid-area: s; position: relative; width: 50%; left: 50%;}
       .off_delay_time { grid-area: o; }
       .hswing {       grid-area: hs; }
       .hswing_angle { grid-area: ha; }
@@ -400,7 +533,7 @@ class fanXiaomiMiotCard extends LitElement {
       .vswing_angle { grid-area: va; }
       .swing_down {   grid-area: va; position: relative; width: 50%; }
       .swing_up {     grid-area: va; position: relative; width: 50%; left: 50%; }
-      .auto_on { grid-area: ao; }
+      .auto_on {      grid-area: ao; }
       .auto_on_temp_minus {grid-area: aot; position: relative; width: 50%; }
       .auto_on_temp_plus  {grid-area: aot; position: relative; width: 50%; left: 50%; }
       .auto_off {     grid-area: af; }
@@ -424,6 +557,10 @@ class fanXiaomiMiotCard extends LitElement {
       .off_delay_time.active .state {
         display: inline;
       }
+      .power .label:after {
+        content: "%";
+        font-size: 80%;
+      }
       .off_delay_time .state:after {
         content: "m";
         font-size: 80%;
@@ -431,6 +568,10 @@ class fanXiaomiMiotCard extends LitElement {
       .vswing_angle .state:after,
       .hswing_angle .state:after {
         content: "°";
+      }
+      .hswing_angle .icon-waper,
+      .vswing_angle .icon-waper {
+        transform: scale(0.7);
       }
       .not-found {
         background-color: yellow;
@@ -442,3 +583,4 @@ class fanXiaomiMiotCard extends LitElement {
   }
 }
 customElements.define("fanXiaomiMiot-card", fanXiaomiMiotCard);
+console.info(`%cFAN-XIAOMI-MIOT v0.0.5 IS INSTALLED`,"color: green; font-weight: bold","");
